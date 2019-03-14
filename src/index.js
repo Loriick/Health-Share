7918f3eadc49b8edf030da67cd44f0fa2975d7ae
Travailleur Lorick <loricktravailleur@gmail.com>
2019-03-14 08:28:34 +0100

    level 3 done: display total of binary after drop

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import "core-js/shim"; // included < Stage 4 proposals
+import "regenerator-runtime/runtime";
 import "./styles/style.scss";
 
 //DOM Element
@@ -14,26 +16,37 @@ const handleDrop = e => {
   const file = e.dataTransfer.files[0];
   const { name } = file;
   uploadFile(file);
-  drop.insertAdjacentHTML("beforeend", renderAfterDrop(name));
+  // drop.insertAdjacentHTML("beforeend", renderAfterDrop(name));
+  renderAfterDrop("Your file is: ", name);
 };
 
-const renderAfterDrop = name => `<p>your file name is ${name}</p>`;
+const renderAfterDrop = (message, data) => {
+  return drop.insertAdjacentHTML("beforeend", `<p>${message} ${data}</p>`);
+};
+
+const getTotalBinary = binary => binary;
 
 eventsArray.forEach(event => dropZone.addEventListener(event, preventDefaults));
 dropZone.addEventListener("drop", handleDrop);
 
-const uploadFile = file => {
+const uploadFile = async file => {
   let formData = new FormData();
   formData.append("file", file);
-
-  fetch("https://fhirtest.uhn.ca/baseDstu3/Binary", {
-    method: "POST",
-    body: formData
-  })
-    .then(data => {
-      console.log(data);
-    })
-    .catch(err => {
-      console.log(err);
+  try {
+    const data = await fetch("https://fhirtest.uhn.ca/baseDstu3/Binary", {
+      method: "POST",
+      body: formData
     });
+    console.log(data);
+    const findTotal = await fetch(
+      "http://hapi.fhir.org/baseDstu3/Binary/_history?"
+    );
+    const findTotalJSON = await findTotal.json();
+    const { total } = findTotalJSON;
+    getTotalBinary(total);
+    console.log(total);
+    renderAfterDrop("Total of Binary", total);
+  } catch (error) {
+    console.log(err);
+  }
 };
